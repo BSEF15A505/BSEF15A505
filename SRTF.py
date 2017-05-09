ed3563814610729b54539126c72a971d0570e463
BSEF15A505 <bsef15a505@pucit.edu.pk>
2017-05-09 06:21:57 +0500

    Update SRTF.py

--- SRTF.py
+++ SRTF.py
@@ -1,41 +1,36 @@
+BT = []  # burst time
+AT = []  # arrival time
 
-BT = [] # burst time
-AT = []   #  arrival time
-WT = 0 # waiting time
-TT = 0 # turnaround time
-
-print('enter total number of process')
+print('Enter total number of process', end = ' ')
 n = int(input())
 
-
 for i in range(n):
-    print('Arrival time of process P',i+1)
+    print('Arrival time of process P', i + 1,  end = ' ')
     AT.append(int(input()))
-    print('Burst time of process P',i+1)
+    print('Burst time of process P', i + 1,  end = ' ')
     BT.append(int(input()))
 
-b=[] # burst time
-for i in range(n):
-    b.append(BT[i])
-
-smallest=0 # shortest job
-b_time=0 # burst time variable
+smallest = 0  # shortest job
+done = [] # done processes
 for j in range(n):
-   if AT[j] < AT[smallest]:
-         smallest=j;
+    if AT[j] < AT[smallest]:
+        smallest = j;
 time = AT[smallest]
-
-i=0
+w_time = 0 # waiting time
+t_time = 0 # turnaroud time
 for i in range(n):
-    smallest = n
     for j in range(n):
-
-        if (AT[j] <= time) and (BT[j] < BT[smallest]) and (BT[j] > 0):
-             smallest = j
-    BT[smallest] = BT[smallest]-1
-    time = time + 1
-    if BT[smallest] is 0:
-        WT = time - AT[smallest] - b[smallest]
-        TT = WT + b[smallest]
-        iA=i+1
-        print('process', smallest+1, ' Waiting_time:', WT, 'Turnaround_time:', TT)
+        if(j==0):
+            if (AT[j] <= time) and (BT[j] < BT[smallest]) and (BT[j] > 0):
+                smallest = j
+        for k in range(len(done)):
+            if(j != done[k]):
+                if (AT[j] <= time) and (BT[j] < BT[smallest]) and (BT[j] > 0):
+                    smallest = j
+        time = time + 1
+        BT[smallest] = BT[smallest] - 1
+        w_time = time - AT[smallest] - BT[smallest]
+        t_time = w_time + BT[smallest]
+        if(BT[smallest] == 0):
+            done.append(smallest)
+        print('process', smallest + 1, ' Waiting_time:', w_time + 1, 'Turnaround_time:', t_time)
